f62d8bc4f38a8f5be5dd6661feadd4faf870841f
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-11-11 22:38:31 -0700

    Add api requirements and lifespan method

--- services/api-gateway/app/main.py
+++ services/api-gateway/app/main.py
@@ -1,14 +1,24 @@
 import os
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.api import info, caption, misc
 
 APP_NAME = os.getenv("APP_NAME") if os.getenv("APP_NAME") else "API Gateway"
 
-app = FastAPI(title=APP_NAME)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Connect redis client
+    # Connect to database
+    print("Connection Setup success")
+    yield
+    # Disconnect redis client
+    # Disconnect from database
+    print("Clean Up complete: shutting down now")
+
+
+app = FastAPI(title=APP_NAME, lifespan=lifespan)
 
 app.include_router(info.router, tags=["info"])
 app.include_router(caption.router, prefix="/caption", tags=["caption"])
 app.include_router(misc.router, tags=["misc"])
-# app.include_router(oauth.router, prefix="/oauth", tags=["oauth"])
-# app.include_router(user.router, prefix="/user", tags=["user"])
-
