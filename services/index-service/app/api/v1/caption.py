90072c6fe004c96aad045713db819714b8a12444
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-12-10 18:24:39 -0700

    Add indexing search endpoint

--- services/index-service/app/api/v1/caption.py
+++ services/index-service/app/api/v1/caption.py
@@ -1,24 +1,35 @@
 from fastapi import APIRouter
+from app.helper.indexer import indexer
+from typing import List
+from pydantic import BaseModel
 
 router = APIRouter()
 
-
+class IndexQuery(BaseModel):
+    k: int
+    embedding_vector: List[float]
+    
 @router.get("/info")
 def caption_database_info():
+    indexer.index.code_size
     # Get caption count
     # Get unique document id count
     return {
-        "caption_count" : 40000,
+        "caption_count" : indexer.index.ntotal,
         "document_source_count": 1000 
     }
 
 
-@router.post("/add")
-def add_caption():
+@router.post("/retrieve-index")
+def search_index(query: IndexQuery):
     # Get embedded representation from embedding service
+    results = indexer.retrieve_caption_index(query.embedding_vector, query.k)
     # Save caption to database, get saved id
+    results = [int(idx) for idx in results]
+    print(results)
+    print(type(results))
     # Send embedded representation index-service
     return {
-        "result" : "success" 
+        "result" : results
     }
 
