9f9be35e225be1892f9ec63aa108b1055b5bf1dd
Pawin Taechoyotin <pawin.taechoyotin@gmail.com>
2023-12-13 01:54:39 -0700

    Update Caption Database Page

---
+++ services/web-interface/src/interface/caption.ts
@@ -0,0 +1,10 @@
+export interface iCaption {
+    caption: string
+    image_path: string
+    document_id: string
+    second_level_dir: string
+    image_type: string
+    index: number
+    first_level_dir: string
+    image_file_exist: boolean
+};
